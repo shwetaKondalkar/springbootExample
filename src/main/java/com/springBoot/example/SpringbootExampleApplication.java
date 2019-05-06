08f1d07b0555aad0feca22318e3e388e9e4a5c99
Shweta Kondalkar <shweta.kondalkar@meganexus.com>
2019-05-06 17:09:43 +0530

    rest service example

---
+++ src/main/java/com/springBoot/example/SpringbootExampleApplication.java
@@ -0,0 +1,14 @@
+package com.springBoot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExampleApplication.class, args);
+	}
+
+}
+
